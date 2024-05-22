ec95ecf75da7418d81615cd03c1ebebf0f927239
IlyasErrarhoute <ilyas.errarhoute@stud.hs-bochum.de>
2024-05-22 19:19:46 +0200

    display imgs but needs style

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,9 @@
 <template>
 
- <h1>ilyas </h1>
- <router-view> </router-view>
+ <div class="page-wrap">
+  <router-view> </router-view>
+
+ </div>
 </template>
 
 <script>
@@ -14,13 +16,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- front-end/src/temp-data.js
+++ front-end/src/temp-data.js
@@ -1,26 +1,26 @@
-import schefferBild from './assets/scheffer.jpg';
-import LutickeBild from './assets/lutticke.jpg';
-import oesingBild from './assets/oesing.jpg';
-import kohnBild from './assets/kohn.jpg';
+import schefferBild from './assets/img/profsPic/prof1.jpg';
+import LutickeBild from './assets/img/profsPic/prof2.jpg';
+import oesingBild from './assets/img/profsPic/prof3.jpg';
+import kohnBild from './assets/img/profsPic/prof4.jpg';
 
 
 
 
-export const products = [{
+export const profs = [{
     id: '1',
     name: 'scheffer',
-    imageName: basilBottle,
+    imageName: schefferBild,
 }, {
     id: '2',
     name: 'lutticke',
-    imageName: lemonBottle,
+    imageName: LutickeBild,
 }, {
     id: '3',
     name: 'Oesing',
-    imageName: garlicBottle,
+    imageName: oesingBild,
 }, {
     id: '4',
     name: 'Kohn',
-    imageName: rosemaryBottle,
+    imageName: kohnBild,
 }
 ];
